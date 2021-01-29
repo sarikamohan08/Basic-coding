c5c886559d7acd1b9d1ea2e032becf93d88868a2
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2021-01-29 22:51:55 +0530

    Add files via upload

--- Searching/maximize the problem.py
+++ Searching/maximize the problem.py
@@ -1,13 +1,11 @@
 n,k=map(int,input().split())
 s=0
-l=[]
-for i in range(1,n+1):
-    l.append(i*5)
-
-
 h=240-k
 count=0
-for i in range(len(l)):
-    if(l[i]<=h):
-        count+=1
+for i in range(n):
+    s+=(i+1)*5
+    if(s<h):
+      count+=1
+    else:
+      break
 print(count)
---
+++ Searching/peak of the house.py
@@ -0,0 +1,20 @@
+def peakElement(arr,n,low,high):
+  
+    mid=low+(high-low)//2
+    if((mid==0 or arr[mid-1]<=arr[mid]) and (mid==n-1 or arr[mid+1]<=arr[mid])):
+        return mid
+    elif(mid>0 and arr[mid-1]>arr[mid]):
+        return peakElement(arr,n,low,mid-1)
+    else:
+        return peakElement(arr,n,mid+1,high)
+
+def main():
+    t=int(input())
+    while(t>0):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        res=(peakElement(arr,n,0,n-1))
+        print(res)
+        t=t-1
+if __name__=='__main__':
+    main()
