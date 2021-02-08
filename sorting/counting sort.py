5f6e38da8c9552462530f3639d080f7281af60f9
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2021-02-08 23:12:39 +0530

    sorting Technique

---
+++ sorting/counting sort.py
@@ -0,0 +1,32 @@
+def countingsort(arr,n,k):
+    freq=[0]*(k+1)
+    for i in range(n):
+        freq[arr[i]]+=1
+
+    count=[0]*(k+1)
+    count[0]=freq[0]
+    for i in range(1,k):
+        count[i]=count[i-1]+freq[i]
+
+    arr1=[None]*n
+    for i in range(n-1,-1,-1):
+        count[arr[i]]-=1
+        arr1[count[arr[i]]]=arr[i]
+    return arr1
+def main():
+    t=int(input())
+    while(t!=0):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        max=-1
+        for i in range(n):
+            if(max<arr[i]):
+                max=arr[i]
+
+        k=max
+        arr=countingsort(arr,n,k)
+        for i in range(n):
+            print(arr[i],end=' ')
+        print()
+        t=t-1
+main()
