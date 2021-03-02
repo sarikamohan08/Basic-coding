1fcef8fca5f57047c60fcfd3ef29a74e847ad9a9
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2021-03-02 23:00:56 +0530

    Add files via upload

---
+++ stacks/bracket 2 expression.py
@@ -0,0 +1,59 @@
+
+class Stack:
+    def __init__(self):
+        self.arr=[]
+        self.tos=-1
+
+    def push(self,data):
+        self.tos+=1
+        self.arr.append(data)
+
+    def popStack(self):
+        if(self.isEmpty()):
+            print("stack is empty")
+            return
+        self.tos=self.tos-1
+        self.arr.pop()
+        
+    def size(self):
+        return self.tos+1
+
+    def isEmpty(self):
+        return self.tos==-1
+
+    def top(self):
+        if(self.isEmpty()):
+            print("stack is empty")
+            return
+        return self.arr[self.tos]
+
+def main():
+    
+    t=int(input())
+    while(t>0):
+        st=Stack() # we need to clear stack before entering other test case
+        str1=input()
+        flag=1
+        for ele in str1:
+            if(ele=="(" or ele=="{" or ele=="["):
+                st.push(ele)
+            else:
+                if(st.isEmpty()):
+                    flag=0
+                    break
+                if((st.top()=='(' and ele==')')or (st.top()=='[' and ele==']')or (st.top()=='{' and ele=='}')):
+                    st.popStack()
+                else:
+                    flag=0
+                    break
+        if(st.isEmpty() is False or flag==0):
+            print("expression is unbalance")
+        else:
+            print("expression is balanced")
+    t=t-1
+
+if __name__=='__main__':
+    main()
+    
+
+
