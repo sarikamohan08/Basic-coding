353e9132fb54188a157ef7023b6fc70ae18a8262
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2021-03-30 22:37:14 +0530

    Add files via upload

---
+++ Binary Tree/node creation.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+def main():
+    b1=Node(1)
+    b2=Node(2)
+    b3=Node(3)
+    b1.left=b2
+    b1.right=b3
+    print(b1,b1.left,b1.right)
+    print(b2)
+    print(b3)
+
+main()
